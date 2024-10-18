5f14bbc1e6a444e6481734c5aa34bf0a2b86edcd
Timothy Lim <contacttimothylim@gmail.com>
2024-10-18 11:50:36 -0400

    commit

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,7 +1,34 @@
-export const getPets = (req, res) => {
-    // logic goes here
+import Pet from "../models/pet.js";
+
+export const getPets = async (req, res) => {
+  try {
+    const pets = await Pet.find({});
+    res.status(200).json(pets);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  
-  export const getPet = (req, res) => {
-    
+};
+
+export const createPet = async (req, res) => {
+  try {
+    const createdPet = await Pet.create(req.body);
+    res.status(201).json(createdPet);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
+};
+
+export const getPet = async (req, res) => {
+    try {
+       const pet = await Pet.findById(req.params.petId);
+       if (!pet) {
+         res.status(404);
+         throw new Error("Pet not found");
+       }
+       res.status(200).json(pet);
+     } catch (error) {
+       res.status(500).json({ error: error.message });
+     }
+};
+export const updatePet = async (req, res) => {};
+export const deletePet = async (req, res) => {};
